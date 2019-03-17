9e1dd63a30f9ebd28ef4a0d946e208fe746d7833
Vu Truong <vu.truong@employmenthero.com>
2019-03-17 20:49:50 +0700

    Add bug reporter config

--- lib/grimes/config.rb
+++ lib/grimes/config.rb
@@ -1,5 +1,9 @@
 module Grimes
   class Config
+    attr_accessor :bug_reporter
 
+    def report_bug(error, params = {})
+      bug_reporter.notify(error, params) if bug_reporter.present?
+    end
   end
 end
