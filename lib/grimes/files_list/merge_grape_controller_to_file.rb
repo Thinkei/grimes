9dc8c475a5891978b1718e8616740315fc83db9c
Tuan Mai <perhuman@gmail.com>
2019-03-21 09:56:20 +0700

    chore/add service to merge grape controller to file

---
+++ lib/grimes/files_list/merge_grape_controller_to_file.rb
@@ -0,0 +1,56 @@
+module FilesList
+  class MergeGrapeControllerToFile
+    attr_reader :controllers, :files
+    def initialize(controllers, files)
+      @controllers = controllers
+      @files = files
+    end
+
+    def merge
+      files.map do |file|
+        controller = find_controller_for_file(file)
+        if controller
+          file = file.merge({
+            extra: {
+              controller: true,
+              actions: controller[:actions]
+            }
+          })
+        end
+        file
+      end
+    end
+
+    private
+
+    def grouped_controllers
+      @grouped_controllers ||= begin
+         results = []
+         controllers.each do |controller|
+           target_controller = find_controller(results, controller)
+           if !target_controller
+             target_controller = default_controller(controller[:controller])
+             results.push(target_controller)
+           end
+           target_controller[:actions] << controller[:action]
+         end
+         results
+       end
+    end
+
+    def find_controller(controllers, controller)
+      find_controller = controllers.find { |c| c[:controller] == controller[:controller] }
+      find_controller
+    end
+
+    def default_controller(controller_path)
+      { controller: controller_path, actions: [] }
+    end
+
+    def find_controller_for_file(file)
+      grouped_controllers.find do |controller|
+        controller[:controller].include?(file[:path])
+      end
+    end
+  end
+end
---
+++ spec/files_list/merge_grape_controller_to_file_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'grimes/files_list/merge_grape_controller_to_file'
+
+describe FilesList::MergeGrapeControllerToFile do
+  let(:controllers) do
+    [
+      {:controller => "/code/app/controllers/model_1_controller.rb", :action => "action_1"},
+      {:controller => "/code/app/controllers/model_1_controller.rb", :action => "action_2"},
+      {:controller => "/code/app/controllers/model_2_controller.rb", :action => "action_3"},
+    ]
+  end
+  let(:files) do
+    [
+      {:path => "app/controllers/model_1_controller.rb"},
+      {:path => "app/controllers/model_2_controller.rb"},
+      {:path => "app/controllers/model_3_controller.rb"},
+    ]
+  end
+  let(:expected_result) do
+    [
+      {
+        :path => "app/controllers/model_1_controller.rb",
+        :extra => {
+          controller: true,
+          actions: ['action_1', 'action_2']
+        }
+      },
+      {
+        :path => "app/controllers/model_2_controller.rb",
+        :extra => {
+          controller: true,
+          actions: ['action_3']
+        }
+      },
+      { :path => "app/controllers/model_3_controller.rb" }
+    ]
+  end
+  let(:subject) { described_class.new(controllers, files) }
+
+  it 'merges controllers to files' do
+    expect(subject.merge).to eq(expected_result)
+  end
+end
