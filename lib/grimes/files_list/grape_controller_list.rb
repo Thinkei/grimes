e13b9dc7d7338442a49011e242eea62464c8cf54
Tuan Mai <perhuman@gmail.com>
2019-03-21 09:49:22 +0700

    chore/add logic to get grape routes

---
+++ lib/grimes/files_list/grape_controller_list.rb
@@ -0,0 +1,17 @@
+# This logic is tight coupling with Grape API, please check the Grape API more details
+
+module FilesList
+  class GrapeControllerList
+    attr_reader :grape_routes
+
+    def initialize(grape_routes)
+      @grape_routes = grape_routes
+    end
+
+    def get_controllers
+      grape_routes.map do |route|
+        route.routes.map {|r| { controller: r.app.source.source_location.first, action: "#{r.request_method} #{r.path}"} }
+      end.flatten
+    end
+  end
+end
