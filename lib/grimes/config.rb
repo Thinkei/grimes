a6522070d71feadaa90dff9d93d4a83195175adc
Vu Truong <vu.truong@employmenthero.com>
2019-03-27 18:30:38 +0700

    Add new config

--- lib/grimes/config.rb
+++ lib/grimes/config.rb
@@ -1,7 +1,8 @@
 module Grimes
   class Config
     attr_accessor :track_controller, :render_partial_block, :render_template_block,
-      :namespace, :rake_task_block, :bug_reporter, :track_grape, :rails_application
+                  :render_controller_block, :namespace, :rake_task_block, :bug_reporter,
+                  :track_grape, :rails_application, :app_root
     attr_writer :track_paths, :ignore_paths, :grape_routes
 
     def track_paths
