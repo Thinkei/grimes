771578455cfc03fbd84e8d494972f5a16b772108
Vu Truong <vu.truong@employmenthero.com>
2019-03-27 18:31:14 +0700

    Finish controller tracker

--- lib/grimes.rb
+++ lib/grimes.rb
@@ -1,6 +1,5 @@
 require 'grimes/config'
 require 'grimes/railtie' if defined?(Rails)
-require 'grimes/rails_extension_extractor'
 
 module Grimes
   class << self
@@ -14,6 +13,10 @@ module Grimes
 
     def track_rails_templates
       require 'grimes/rails_template_tracker'
+    end
+
+    def track_rails_controllers
+      require 'grimes/rails_controller_tracker'
     end
   end
 end
