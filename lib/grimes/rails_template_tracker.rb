3fc35f9f4244b24d6d2442d5c547b87980242c43
Vu Truong <vu.truong@employmenthero.com>
2019-05-16 10:41:21 +0700

    Change tracking trigger logic for rails views and controllers

--- lib/grimes/rails_template_tracker.rb
+++ lib/grimes/rails_template_tracker.rb
@@ -1,48 +1,47 @@
 module ActionView
-  class LogSubscriber
-    alias original_render_template render_template
-    alias original_render_partial render_partial
-
-    def render_template(event)
-      begin
-        template_file = event.payload[:identifier].sub(rails_root, EMPTY)
-        callback_block = Grimes.config.render_template_block
-        callback_block&.call(file_path: template_file)
-        original_render_template(event)
-      rescue StandardError => error
-        Grimes.config.report_bug(error)
-        original_render_template(event)
-      end
-    end
-
-    def render_partial(event)
-      begin
-        template_file = event.payload[:identifier].sub(rails_root, EMPTY)
-        callback_block = Grimes.config.render_partial_block
-        callback_block&.call(file: template_file)
-        original_render_partial(event)
-      rescue StandardError => error
-        Grimes.config.report_bug(error)
-        original_render_partial(event)
-      end
-    end
-  end
-
   class TemplateRenderer
-    alias original_find_layout find_layout
+    alias original_render_template render_template
 
     private
+
+    def render_template(template, layout_name = nil, locals = nil)
+      begin
+        template_path = template.identifier.sub(Grimes.config.app_root, '')
+        callback_block = Grimes.config.render_template_block
+        callback_block&.call(file_path: template_path)
+        original_render_template(template, layout_name, locals)
+      rescue StandardError => error
+        Grimes.config.report_bug(error)
+        original_render_template(template, layout_name, locals)
+      end
+    end
 
     def find_layout(layout, keys)
       begin
         layout = with_layout_format { resolve_layout(layout, keys) }
         callback_block = Grimes.config.render_template_block
-        callback_block&.call(file: layout.inspect)
+        callback_block&.call(file_path: layout.inspect)
         layout
       rescue StandardError => error
         Grimes.config.report_bug(error)
         original_find_layout(layout, keys)
       end
+    end
+  end
+
+  class PartialRenderer
+    alias original_render render
+
+    def render(context, options, block)
+      result = original_render(context, options, block)
+      identifier = (@template = find_partial) ? @template.identifier : @path
+      template_path = identifier.sub(Grimes.config.app_root, '')
+      callback_block = Grimes.config.render_partial_block
+      callback_block&.call(file_path: template_path)
+      result
+    rescue StandardError => error
+      Grimes.config.report_bug(error)
+      original_render(context, options, block)
     end
   end
 end
